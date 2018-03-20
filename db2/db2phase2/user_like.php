ffc8c4ff3d80014b38d1186c0c5702ec4ae1af69
emmanrosa <emmanuel_rosario@student.uml.edu>
2018-03-20 17:45:06 -0400

    likes

--- db2/db2phase2/user_like.php
+++ db2/db2phase2/user_like.php
@@ -12,22 +12,20 @@ if(isset($user_uid)){
 $user_uid = $_SESSION['uid'];
 }
 
-
- 
 	if( isset($_GET['lik']) )
 	{
 	$lik_tid = $_GET['lik'];
 	// check for duplicates 
 	// for  likes
-	$get_num_like = "SELECT `count(*)` FROM `thumb` WHERE `uid` = $user_uid AND `tid` = $lik_tid";
+	$get_num_like = "SELECT count(*) FROM `thumb` WHERE `uid` = $user_uid AND `tid` = $lik_tid";
 	$check_for_like = mysqli_query($conn,$get_num_like);
 
 	//
 	//
-	$get_num_dislike = "SELECT `count(*)` FROM `dislike` WHERE `uid` = $user_uid AND `tid` = $lik_tid";
+	$get_num_dislike = "SELECT count(*) FROM `dislike` WHERE `uid` = $user_uid AND `tid` = $lik_tid";
 	$check_for_dislike = mysqli_query($conn,$get_num_dislike);
 
-		if (mysqli_num_rows($check_for_like) == 1) {
+		if (mysqli_num_rows($check_for_like) > 0) {
 			echo "<meta http-equiv='refresh' content='0;url=home.php'>";
 		}elseif (mysqli_num_rows($check_for_dislike) > 0) {
 
